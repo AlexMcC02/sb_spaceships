74309e777b982b644822bae317f8ebd8a1060c69
AlexMcC02 <alex.mccullough@kainos.com>
2023-08-15 15:32:47 +0100

    refactor: implemented lombok for Captain class

--- src/main/java/com/example/spaceships/models/Captain.java
+++ src/main/java/com/example/spaceships/models/Captain.java
@@ -1,43 +1,15 @@
 package com.example.spaceships.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class Captain {
     private int id;
     private String name;
     private int age;
     private Rank rank;
-
-    public Captain(int id, String name, int age, Rank rank) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.rank = rank;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Rank getRank() {
-        return rank;
-    }
-
-    public void setRank(Rank rank) {
-        this.rank = rank;
-    }
 }
