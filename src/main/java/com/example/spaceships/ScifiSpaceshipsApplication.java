4c0e68756a8cb561f8096e9b1db614ce9373b138
AlexMcC02 <alex.mccullough@kainos.com>
2023-08-15 15:17:33 +0100

    init commit

---
+++ src/main/java/com/example/spaceships/ScifiSpaceshipsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spaceships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScifiSpaceshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScifiSpaceshipsApplication.class, args);
+	}
+
+}
